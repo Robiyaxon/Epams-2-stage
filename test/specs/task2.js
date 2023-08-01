5ca08afd3094ce6774933f09c281a7b3235672d9
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-01 18:57:36 +0500

    done all task

--- test/specs/task2.js
+++ test/specs/task2.js
@@ -30,17 +30,77 @@ async function automateScript() {
   );
   await driver.switchTo().frame(iframe2);
 
-  // const select2 = await driver.wait(
-  //   until.elementLocated(By.xpath("//input[@id='input-0']"))
-  // );
-  // select2.click()
-  // select2.sendKeys("Compute Engine")
-  // select2.click()
   //
+  const engine = await driver.wait(
+    until.elementLocated(By.xpath("//input[@id='input-0']"))
+  );
+  await driver.actions().click(engine).perform();
+  await driver.actions().sendKeys("Compute Engine").perform();
+  await driver.sleep(2000);
+  await driver.actions().sendKeys(Key.ARROW_DOWN).perform();
+  await driver.actions().sendKeys(Key.ENTER).perform();
+  //
+
   const quantityInput = await driver.wait(
     until.elementLocated(By.id(`input_98`))
   );
   await quantityInput.click();
   await quantityInput.sendKeys("4");
+
+  //
+
+  const operatingSystem = await driver.wait(until.elementLocated(By.id(`select_111`)));
+  await operatingSystem.click();
+  const operatingSystemOption = await driver.wait(
+    until.elementLocated(By.id(`select_option_104`))
+  );
+  await operatingSystemOption.click();
+
+  //
+
+  const instanceType = await driver.wait(until.elementLocated(By.id(`select_125`)));
+  await instanceType.click();
+  const instanceTypeOption = await driver.wait(
+    until.elementLocated(By.id(`select_option_285`))
+  );
+  await driver.wait(
+    until.elementIsVisible(driver.findElement(By.id(`select_option_285`))),
+    5000
+  );
+  await instanceTypeOption.sendKeys(Key.ENTER);
+  //
+
+  const localSSD = await driver.wait(until.elementLocated(By.id(`select_131`)));
+  await localSSD.click();
+  const localSsdOption = await driver.wait(
+    until.elementLocated(By.id(`select_option_256`))
+  );
+  await driver.wait(
+    until.elementIsVisible(driver.findElement(By.id(`select_option_256`))),
+    5000
+  );
+  await localSsdOption.sendKeys(Key.ENTER);
+
+  //
+
+  const datacenterLocation = await driver.wait(until.elementLocated(By.id(`select_138`)));
+  await datacenterLocation.click();
+  const datacenterLocationOption = await driver.wait(
+    until.elementLocated(By.id(`select_option_136`))
+  );
+  await driver.wait(
+    until.elementIsVisible(driver.findElement(By.id(`select_option_136`))),
+    5000
+  );
+  await datacenterLocationOption.sendKeys(Key.ENTER);
+
+  const clickButton = await driver.wait(
+    until.elementLocated(
+      By.xpath(
+        "//form[@name='ComputeEngineForm']//button[@type='button'][normalize-space()='Add to Estimate']"
+      )
+    )
+  );
+  await clickButton.click();
 }
 automateScript();

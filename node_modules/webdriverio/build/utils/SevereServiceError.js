ca4afb2d364fe50c177990eb2a5642c445377a59
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-24 13:56:25 +0500

    fixed

--- node_modules/webdriverio/build/utils/SevereServiceError.js
+++
@@ -1,9 +0,0 @@
-/**
- * Error to be thrown when a severe error was encountered when a Service is being ran.
- */
-export default class SevereServiceError extends Error {
-    constructor(message = 'Severe Service Error occurred.') {
-        super(message);
-        this.name = 'SevereServiceError';
-    }
-}

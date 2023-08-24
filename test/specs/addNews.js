f9e838560374a674d83e6a344b2ef4b89c8b0a35
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-24 13:43:49 +0500

    fixed something

--- test/specs/addNews.js
+++ test/specs/addNews.js
@@ -2,5 +2,8 @@ import AddNews from "../pageobjects/addNews.page";
 describe("My Login application", () => {
   it("should login with valid credentials", async () => {
     await AddNews.login("tomsmith", "SuperSecretPassword!");
+
+
+    
   });
 });

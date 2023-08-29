ccdd131bb43c07156c58e9637ab44dfe4707e9b3
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-08-29 22:47:57 +0500

    fixed some code and done full project!

--- test/specs/pastabinTask.js
+++ test/specs/pastabinTask.js
@@ -4,4 +4,23 @@ describe("Pestbin service", () => {
     await pastebinPage.open();
     await pastebinPage.addnewPaste("Hello from WebDriver", "helloweb");
   });
+
+  //
+  
+  it("should open home page and add syntax", async () => {
+    let name = `git config --global user.name "New Sheriff in Town"
+    git reset $(git commit-tree`;
+    let syntax = "Bash";
+    let text = "Pastebin.com - #1 paste tool since 2002!";
+
+    await pastebinPage.open();
+    await pastebinPage.addnewPaste(name, text, syntax);
+
+    let titleInput = await browser.getTitle();
+    await expect(titleInput).toEqual(text);
+    let text_syntaxType = await $(
+      "//span[@id='select2-postform-format-container']"
+    ).getText();
+    await expect(text_syntaxType).toEqual(syntax);
+  });
 });
